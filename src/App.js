27ac20977c120f588659fa59445ff80dd8f060ea
Varsha <varshabhadana93@gmail.com>
2023-04-13 00:21:30 +0200

    Add feature to create new book

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+import BookEdit from './components/BookEdit';
+import BookList from './components/BookList';
+import BookShow from './components/BookShow';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    setBooks([
+      ...books,
+      { id: Math.round(Math.random() * 9999) + 1, title: title },
+    ]);
+    console.log('click', title);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {books.length}
+      <BookCreate onCreate={createBook} />
+      <BookEdit />
+      <BookList />
+      <BookShow />
     </div>
   );
 }
