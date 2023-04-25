c429849c8ed1f02c578f00f37347929249665baa
Varsha <varshabhadana93@gmail.com>
2023-04-25 23:36:18 +0200

    Add data persistence with API request

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
+  const fetchBook = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
 
-  const deleteBookById = (id) => {
+  useEffect(() => {
+    fetchBook();
+  }, []);
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
-  const editBookById = (id, title) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: title };
+        return { ...book, ...response.data };
       }
       return book;
     });
     setBooks(updatedBooks);
   };
-  const createBook = (title) => {
-    setBooks([
-      ...books,
-      { id: Math.round(Math.random() * 9999) + 1, title: title },
-    ]);
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+    const updatedBooks = [...books, response.data];
+    setBooks(updatedBooks);
   };
+
   return (
     <div className="app">
       <h1>Reading List</h1>
